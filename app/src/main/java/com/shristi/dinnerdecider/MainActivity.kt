18ef2d0cb8ada7ac082e5dbdb77bbb6164025820
Shristi <shristigautam90@gmail.com>
2022-10-19 23:04:15 -0500

    almost complete

--- app/src/main/java/com/shristi/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/shristi/dinnerdecider/MainActivity.kt
@@ -2,11 +2,30 @@ package com.shristi.dinnerdecider
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.collections.random as random
 
 class MainActivity : AppCompatActivity() {
-    var myArrayList = arrayListOf("Hamburger", "Pizza", "Mexican", "American", "Chinese")
+    var foodList: ArrayList<String> = arrayListOf<String>("Hamburger", "Pizza", "Mexican", "American", "Chinese")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //video note: interaction between the activity and layout
         setContentView(R.layout.activity_main)
+
+        addFood.setOnClickListener{
+            foodList.add(editTextFoodName.text.toString())
+            editTextFoodName.setText("")
+        }
     }
+
+    fun decide(view: View) {
+//        (editTextFoodName as TextView).text = foodList.random()
+        editTextFoodName.setText(foodList.random())
+    }
+
 }
