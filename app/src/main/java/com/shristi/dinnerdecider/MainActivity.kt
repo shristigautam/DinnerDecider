f95a2effd6cbad0b01546188eea95de672582a41
Shristi <shristigautam90@gmail.com>
2022-10-19 23:26:08 -0500

    Finished

--- app/src/main/java/com/shristi/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/shristi/dinnerdecider/MainActivity.kt
@@ -7,18 +7,22 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.collections.random as random
 
 class MainActivity : AppCompatActivity() {
-    var foodList: ArrayList<String> = arrayListOf<String>("Hamburger", "Pizza", "Mexican", "American", "Chinese")
+    var foodList: ArrayList<String> =
+        arrayListOf<String>("Hamburger", "Pizza", "Mexican", "American", "Chinese")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //video note: interaction between the activity and layout
         setContentView(R.layout.activity_main)
 
-        addFood.setOnClickListener{
+        addFood.setOnClickListener {
             foodList.add(editTextFoodName.text.toString())
+            showAlert()
             editTextFoodName.setText("")
         }
     }
@@ -26,6 +30,18 @@ class MainActivity : AppCompatActivity() {
     fun decide(view: View) {
 //        (editTextFoodName as TextView).text = foodList.random()
         editTextFoodName.setText(foodList.random())
+    }
+
+    fun showAlert() {
+        val dialogBuilder = AlertDialog.Builder(this)
+        // set message of alert dialog
+        dialogBuilder.setMessage("New food successfully added to list!").setCancelable(true)
+        // create dialog box
+        val alert = dialogBuilder.create()
+        // set title for alert dialog box
+        alert.setTitle("Food added!")
+        // show alert dialog
+        alert.show()
     }
 
 }
